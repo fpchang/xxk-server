f0f3bc1a3255be45317c7ee9af834af2a99ee515
fpchang <hanfengcfp@outlook.com>
2025-03-27 16:49:48 +0800

    add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
